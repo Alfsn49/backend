0f44f85f8f3b078126c590abb2d318e554754005
Alfsn49 <edgar.amanta@espoch.edu.ec>
2024-01-19 23:55:41 -0500

    Registro

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import RegisterPage from './pages/RegisterPage'
-import LoginPage from './pages/LoginPage'
-import HomePage from './pages/HomePage'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import { AuthProvider } from "./context/AuthContext";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/register" element={<RegisterPage/>} />
-        <Route path="/tasks" element={<h1>Tasks</h1>} />
-        <Route path="/add-task" element={<h1>New task</h1>} />
-        <Route path="/tasks/:id" element={<h1>Update Tasks</h1>} />
-        <Route path="/profile" element={<h1>Profile</h1>} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/tasks" element={<h1>Tasks</h1>} />
+          <Route path="/add-task" element={<h1>New task</h1>} />
+          <Route path="/tasks/:id" element={<h1>Update Tasks</h1>} />
+          <Route path="/profile" element={<h1>Profile</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
